61e2d433a235ae8f6aa9b7153dcd829daaf3c981
GGG <kuranberitorappu@gmail.com>
2022-04-15 06:28:45 +0000

    propertiesテーブル作成

---
+++ db/migrate/20220415062414_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.integer :price
+      t.string :address
+      t.integer :age
+      t.text :remarks
+      t.timestamps
+    end
+  end
+end
