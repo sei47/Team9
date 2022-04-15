d3b486b8cb17aae1b13ce191eb17f36467f64861
GGG <kuranberitorappu@gmail.com>
2022-04-15 08:37:52 +0000

    stationsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_062414) do
+ActiveRecord::Schema.define(version: 2022_04_15_083125) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_04_15_062414) do
     t.string "address"
     t.integer "age"
     t.text "remarks"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "root_name"
+    t.string "station_name"
+    t.integer "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
