40c8b282f3f62b83f6181bfed84e25e5179bea24
GGG <kuranberitorappu@gmail.com>
2022-04-19 08:34:29 +0000

    バリデーション追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :remarks, presence: true
 end
